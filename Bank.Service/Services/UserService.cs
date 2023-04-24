5598204f4b425bf5d52bc36b34b407601c12ee91
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-24 17:53:59 +0500

    added AddAsyncmethod

--- Bank.Service/Services/UserService.cs
+++ Bank.Service/Services/UserService.cs
@@ -3,6 +3,7 @@ using Bank.Data.IRepository;
 using Bank.Domain.Configuration;
 using Bank.Domain.Entites;
 using Bank.Service.DTOs;
+using Bank.Service.Exceptions;
 using Bank.Service.Interfaces;
 using System.Linq.Expressions;
 
@@ -18,22 +19,35 @@ namespace Bank.Service.Services
             this.repository = repository;
         }
 
-        public Task<UserForResultDto> AddAsync(UserForCreationDto dto)
+        public async Task<UserForResultDto> AddAsync(UserForCreationDto dto)
         {
-            throw new NotImplementedException();
+            User user = await this.repository.SelectAsync(
+                u => u.FirstaName == dto.FirstaName && u.LastaName == dto.LastaName);
+            if (user is not null)
+                throw new CustomException(403, "User already exist with this username");
+
+            User mappedUser = mapper.Map<User>(dto);
+            mappedUser.CreatedAt = DateTime.UtcNow;
+            var result = await this.repository.InsertAsync(mappedUser);
+            await this.repository.SaveChangesAsync();
+            return this.mapper.Map<UserForResultDto>(result);
+
         }
 
         public Task<UserForResultDto> ModifyAsync(UserForResultDto dto)
         {
-            throw new NotImplementedException();
+
         }
 
-        public Task<bool> RemoveAsync(long id)
+        public async Task<bool> RemoveAsync(long id)
         {
-            throw new NotImplementedException();
+            var user = await this.repository.SelectAsync(u => u.Id.Equals(id));
+            if (user is null)
+                throw new CustomException(404, "User not found");
         }
 
-        public Task<List<UserForResultDto>> SelectAllAsync(PaginationParams @params, Expression<Func<User, bool>> expression = null, string search = null)
+        public Task<List<UserForResultDto>> SelectAllAsync(
+            PaginationParams @params, Expression<Func<User, bool>> expression = null, string search = null)
         {
             throw new NotImplementedException();
         }
