82ea77fe198c3edb46c7ff5f927199915cbcb9fe
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-24 17:43:31 +0500

    opened new user service

--- Bank.Service/Interfaces/IUserService.cs
+++ Bank.Service/Interfaces/IUserService.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bank.Domain.Configuration;
+using Bank.Domain.Entites;
+using Bank.Service.DTOs;
+using System.Linq.Expressions;
 
 namespace Bank.Service.Interfaces
 {
-    internal class IUserService
+    public interface IUserService
     {
+        Task<UserForResultDto> AddAsync(UserForCreationDto dto);
+        Task<UserForResultDto> ModifyAsync(UserForResultDto dto);
+        Task<bool> RemoveAsync(long id);
+        Task<UserForResultDto> SelectAsync(long id);
+        Task<List<UserForResultDto>> SelectAllAsync(
+            PaginationParams @params, Expression<Func<User, bool>> expression = null, string search = null);
     }
 }
--- Bank.Service/Services/UserService.cs
+++ Bank.Service/Services/UserService.cs
@@ -1,6 +1,38 @@
-﻿namespace Bank.Service.Services
+﻿using Bank.Domain.Configuration;
+using Bank.Domain.Entites;
+using Bank.Service.DTOs;
+using Bank.Service.Interfaces;
+using System.Linq.Expressions;
+
+namespace Bank.Service.Services
 {
-    internal class UserService
+    public class UserService : IUserService
     {
+
+
+        public Task<UserForResultDto> AddAsync(UserForCreationDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserForResultDto> ModifyAsync(UserForResultDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserForResultDto>> SelectAllAsync(PaginationParams @params, Expression<Func<User, bool>> expression = null, string search = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserForResultDto> SelectAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
