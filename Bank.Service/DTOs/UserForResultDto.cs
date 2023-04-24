551ee39824e50a9092ed3729b38799b9b05fd5c8
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-24 16:45:52 +0500

    user for result dto

---
+++ Bank.Service/DTOs/UserForResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank.Service.DTOs
+{
+    public class UserForResultDto
+    {
+        public long Id { get; set; }
+        public string FirstaName { get; set; }
+        public string LastaName { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
