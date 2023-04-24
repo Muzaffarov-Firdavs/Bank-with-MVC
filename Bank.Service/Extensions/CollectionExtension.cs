abcfb2a8e746ee3dc8a5293cf9bd6cd21882c753
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-24 17:02:07 +0500

    added pagination params

---
+++ Bank.Service/Extensions/CollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace Bank.Service.Extensions
+{
+    public static class CollectionExtension
+    {
+        public static IQueryable<T> ToPagedList<T>(this IQueryable<T> source, PaginationParams @params)
+        {
+            int numberOfItemsToSkip = (@params.PageIndex - 1) * @params.PageSize;
+            int totalCount = source.Count();
+
+            if (numberOfItemsToSkip >= totalCount && totalCount > 0)
+            {
+                numberOfItemsToSkip = totalCount - totalCount % @params.PageSize;
+            }
+
+            return source.Skip(numberOfItemsToSkip).Take(@params.PageSize);
+        }
+    }
+}
