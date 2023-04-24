cb1dae8d3e7007d132448e19a477e5fbb82c6bd1
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-24 17:30:55 +0500

    Added mapper

---
+++ Bank.Service/Mappers/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Bank.Domain.Entites;
+using Bank.Service.DTOs;
+
+namespace Bank.Service.Mappers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<UserForCreationDto, User>().ReverseMap();
+            CreateMap<UserForResultDto, User>().ReverseMap();   
+        }
+    }
+}
