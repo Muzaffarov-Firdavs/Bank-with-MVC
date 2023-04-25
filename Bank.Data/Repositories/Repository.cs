263083c7ff20dc32f9d1dbdc984a8997bfe14f85
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-25 10:47:56 +0500

    added dependency to repositry

---
+++ Bank.Data/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Bank.Data.DbContexts;
+using Bank.Data.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Bank.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext dbContext;
+        private readonly DbSet<TEntity> dbSet;
+
+
+        public Repository(AppDbContext dbContext, DbSet<TEntity> dbSet)
+        {
+            this.dbContext = dbContext;
+            this.dbContext.Set<TEntity>();
+        }
+
+        public Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> InsertAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
